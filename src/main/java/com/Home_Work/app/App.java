96188214241f8370dccacf5bee4f033587b100e4
seemeen3900 <seemeen3900@gmail.com>
2017-12-28 19:21:04 -0500

    First Commit

---
+++ src/main/java/com/Home_Work/app/App.java
@@ -0,0 +1,13 @@
+package com.Home_Work.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
